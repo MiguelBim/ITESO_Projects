d6b78ba36134cc0c5f740534a714b8eca3969871
Miguel Ojeda <46506567+MiguelBim@users.noreply.github.com>
2023-03-22 16:30:42 -0600

    Adding python file for image loading to AWS S3

---
+++ Ada Byron (smart cities)/Competencia/Reto Ada Byron 6/Load images to S3.py
@@ -0,0 +1,16 @@
+import boto3
+
+s3 = boto3.resource('s3')
+
+# Get list of objects for indexing
+images=[('image1.jpg','description1'),
+      ('image2.jpg','description2'),
+      ('image3.jpg','description3')
+      ]
+
+# Iterate through list to upload objects to S3   
+for image in images:
+    file = open(image[0],'rb')
+    object = s3.Object('my-bucket-for-images-miguel-ojeda','index/'+ image[0])
+    ret = object.put(Body=file,
+                    Metadata={'FullName':image[1]})
