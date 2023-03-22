68180517117306d267ebd755a81eaff326c420ae
Miguel Ojeda <46506567+MiguelBim@users.noreply.github.com>
2023-03-22 16:41:06 -0600

    Update Load images to S3.py

--- Ada Byron (smart cities)/Competencia/Reto Ada Byron 6/Load images to S3.py
+++ Ada Byron (smart cities)/Competencia/Reto Ada Byron 6/Load images to S3.py
@@ -11,6 +11,6 @@ images=[('image1.jpg','description1'),
 # Iterate through list to upload objects to S3   
 for image in images:
     file = open(image[0],'rb')
-    object = s3.Object('my-bucket-for-images-miguel-ojeda','index/'+ image[0])
+    object = s3.Object('my-bucket-for-images-miguel-ojeda','Imagenes cargadas con codigo/'+ image[0])
     ret = object.put(Body=file,
                     Metadata={'FullName':image[1]})
