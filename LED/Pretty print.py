f744538adc0f2ae8f0f8ef54d2b42fcfe1a6b909
Miguel Ojeda <46506567+MiguelBim@users.noreply.github.com>
2023-05-17 10:20:27 -0600

    Update Pretty print.py

--- LED/Pretty print.py
+++ LED/Pretty print.py
@@ -5,5 +5,5 @@ df = pd.DataFrame({'Nombre' : ["María", "Lupe", "Omar", "Antonia", "Checo"],
                    'Edad' : [21, 20, 22, 19, 20],
                    'Código' : ["c23","c24","c25","c26","c27",]})
 
-# Tabulate library for pandas
+# Tabulate library for pandas, giving a frame to
 print(tabulate(df, headers='keys', tablefmt='psql'))
